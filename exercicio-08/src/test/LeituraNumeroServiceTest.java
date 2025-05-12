2a1762acabf1e46eec5ceb8c752a46eb1e16b4e5
brunna.dornelles <brunnadornelles336@gmail.com>
2025-05-12 11:36:40 -0300

    Configuração de sources roots e test root

--- exercicio-08/src/test/LeituraNumeroServiceTest.java
+++ exercicio-08/src/test/LeituraNumeroServiceTest.java
@@ -1,12 +1,12 @@
 package test;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import service.LeituraNumeroService;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LeituraNumeroServiceTest {
 
